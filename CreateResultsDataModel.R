02feff644c317ae25d41ac8342da08e5cade5699
Anthony Sena <asena5@its.jnj.com>
2025-08-15 14:44:25 -0400

    Updates for results uploading

--- CreateResultsDataModel.R
+++ CreateResultsDataModel.R
@@ -12,9 +12,9 @@
 # ##############################################################################
 
 # Code for creating the result schema and tables in a PostgreSQL database
-resultsDatabaseSchema <- "results"
+resultsDatabaseSchema <- "semanvamd"
 analysisSpecifications <- ParallelLogger::loadSettingsFromJson(
-  fileName = "inst/sampleStudy/sampleStudyAnalysisSpecification.json"
+  fileName = "inst/semaglutideNvamdAnalysisSpecification.json"
 )
 
 resultsDatabaseConnectionDetails <- DatabaseConnector::createConnectionDetails(
@@ -38,3 +38,27 @@ Strategus::createResultDataModel(
   resultsDataModelSettings = resultsDataModelSettings,
   resultsConnectionDetails = resultsDatabaseConnectionDetails
 )
+
+# Used when reloading from scratch:
+truncateAllTables <- function(resultsDatabaseConnectionDetails, resultsDatabaseSchema) {
+  connection <- DatabaseConnector::connect(resultsDatabaseConnectionDetails)
+  on.exit(DatabaseConnector::disconnect(connection))
+  
+  # Analyze all tables in the results schema
+  message("Truncate all tables in results schema")
+  sql <- "TRUNCATE TABLE @schema.@table_name CASCADE;"
+  tableList <- DatabaseConnector::getTableNames(
+    connection = connection,
+    databaseSchema = resultsDatabaseSchema
+  )
+  for (i in 1:length(tableList)) {
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      schema = resultsDatabaseSchema,
+      table_name = tableList[i],
+      progressBar = FALSE,
+      reportOverallTime = FALSE
+    )
+  }
+}
--- UploadResults.R
+++ UploadResults.R
@@ -19,9 +19,9 @@
 # ##############################################################################
 
 # Code for uploading results to a Postgres database
-resultsDatabaseSchema <- "results"
+resultsDatabaseSchema <- "semanvamd"
 analysisSpecifications <- ParallelLogger::loadSettingsFromJson(
-  fileName = "inst/sampleStudy/sampleStudyAnalysisSpecification.json"
+  fileName = "inst/semaglutideNvamdAnalysisSpecification.json"
 )
 resultsDatabaseConnectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "postgresql",
@@ -42,12 +42,14 @@ ParallelLogger::addDefaultErrorReportLogger(
 )
 
 # Upload Results ---------------------------------------------------------------
-for (resultFolder in list.dirs(path = "results", full.names = T, recursive = F)) {
+resultsFolders <- list.dirs(path = "results", full.names = T, recursive = F)
+#resultsFolders <- c("results/washu", "results/jhu", "results/usc")
+for (resultFolder in resultsFolders) {
   resultsDataModelSettings <- Strategus::createResultsDataModelSettings(
     resultsDatabaseSchema = resultsDatabaseSchema,
     resultsFolder = file.path(resultFolder, "strategusOutput"),
   )
-  
+  cli::cli_alert_info(paste0("Uploading ", resultFolder))
   Strategus::uploadResults(
     analysisSpecifications = analysisSpecifications,
     resultsDataModelSettings = resultsDataModelSettings,
@@ -61,43 +63,43 @@ connection <- DatabaseConnector::connect(
 
 
 # Optional scripts to set permissions and to analyze tables ------------------
-# # Grant read only permissions to all tables
-# sql <- "GRANT USAGE ON SCHEMA @schema TO @results_user;
-# GRANT SELECT ON ALL TABLES IN SCHEMA @schema TO @results_user; 
-# GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA @schema TO @results_user;"
-# 
-# message("Setting permissions for results schema")
-# sql <- SqlRender::render(
-#   sql = sql, 
-#   schema = resultsDatabaseSchema,
-#   results_user = 'shinyproxy'
-# )
-# DatabaseConnector::executeSql(
-#   connection = connection, 
-#   sql = sql,
-#   progressBar = FALSE,
-#   reportOverallTime = FALSE
-# )
-#   
-# # Analyze all tables in the results schema
-# message("Analyzing all tables in results schema")
-# sql <- "ANALYZE @schema.@table_name;"
-# tableList <- DatabaseConnector::getTableNames(
-#   connection = connection,
-#   databaseSchema = resultsDatabaseSchema
-# )
-# for (i in 1:length(tableList)) {
-#   DatabaseConnector::renderTranslateExecuteSql(
-#     connection = connection,
-#     sql = sql,
-#     schema = resultsDatabaseSchema,
-#     table_name = tableList[i],
-#     progressBar = FALSE,
-#     reportOverallTime = FALSE
-#   )
-# }
-# 
-# DatabaseConnector::disconnect(connection)
+# Grant read only permissions to all tables
+sql <- "GRANT USAGE ON SCHEMA @schema TO @results_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA @schema TO @results_user;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA @schema TO @results_user;"
+
+message("Setting permissions for results schema")
+sql <- SqlRender::render(
+  sql = sql,
+  schema = resultsDatabaseSchema,
+  results_user = 'shinyproxy'
+)
+DatabaseConnector::executeSql(
+  connection = connection,
+  sql = sql,
+  progressBar = FALSE,
+  reportOverallTime = FALSE
+)
+
+# Analyze all tables in the results schema
+message("Analyzing all tables in results schema")
+sql <- "ANALYZE @schema.@table_name;"
+tableList <- DatabaseConnector::getTableNames(
+  connection = connection,
+  databaseSchema = resultsDatabaseSchema
+)
+for (i in 1:length(tableList)) {
+  DatabaseConnector::renderTranslateExecuteSql(
+    connection = connection,
+    sql = sql,
+    schema = resultsDatabaseSchema,
+    table_name = tableList[i],
+    progressBar = FALSE,
+    reportOverallTime = FALSE
+  )
+}
+
+DatabaseConnector::disconnect(connection)
 
 # Unregister loggers -----------------------------------------------------------
 ParallelLogger::unregisterLogger("RESULTS_FILE_LOGGER")
